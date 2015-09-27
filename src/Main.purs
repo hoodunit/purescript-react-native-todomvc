04305b629d87d919941d9fa9fe6a76b5a80ba108
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2015-09-27 23:01:14 +0300

    Show list of todos

--- src/Main.purs
+++ src/Main.purs
@@ -3,12 +3,23 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Console (log)
-import React (Render(), createClass, spec)
+import React (ReactElement(), Render(), createClass, spec)
 import ReactNative (StyleId(), StyleSheet(), registerComponent, createStyleSheet, getStyleId)
-import ReactNative.Components (text, view)
+import ReactNative.Components (ListViewDataSource(), listView, listViewDataSource, text, view)
+import ReactNative.Props (renderRow, dataSource)
 
 import qualified React.DOM as D
 import qualified React.DOM.Props as P
+
+data Todo = Todo String Boolean
+
+initialTodos = [
+  Todo "Get milk" false,
+  Todo "Return library books" false,
+  Todo "Drink water" true,
+  Todo "Return papers" false,
+  Todo "Call your mother" false
+]
 
 appName = "PureScriptSampleApp"
 
@@ -16,35 +27,64 @@ appStyleSheet :: StyleSheet
 appStyleSheet = createStyleSheet {
   "container": {
      flex: 1,
-     justifyContent: "center",
-     alignItems: "center",
-     backgroundColor: "#F5FCFF"
+     flexDirection: "column",
+     justifyContent: "center"
      },
-  "welcome": {
-     fontSize: 20,
-     textAlign: "center",
-     margin: 10
+  "title": {
+     fontSize: 40,
+     textAlign: "center"
      },
-  "instructions": {
-     textAlign: "center",
-     color: "#333333",
-     marginBottom: 5
-     }
+  "todoList": {
+    flex: 1,
+    flexDirection: "column"
+    },
+  "todoRow": {
+    flexDirection: "column",
+    height: 40,
+    padding: 10,
+    marginBottom: 10
+    },
+  "todoText": {
+    fontSize: 24
+    },
+  "separator": {
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    height: 1,
+    marginVertical: 10
+    }
   }
   
 appStyle :: String -> P.Props
 appStyle key = P.unsafeMkProps "style" $ getStyleId appStyleSheet key
 
-render :: forall props state eff. Render props state eff
-render ctx = pure (view [(appStyle "container")] [
-  (text [(appStyle "welcome")] [D.text "Welcome to React Native (in PureScript)!"]),
-  (text [(appStyle "instructions")] [D.text "To get started, edit src/Main.purs"]),
-  (text [(appStyle "instructions")] [D.text "Shake or press menu button for dev menu"])])
-  
+todoRow :: String -> Int -> Int -> Int -> ReactElement
+todoRow rowData sectionId rowId highlightRow = 
+  view [appStyle "todoRow"] [
+    text [appStyle "todoText"] [D.text rowData],
+    view [appStyle "separator"] []]
 
+todoDataSource :: Array Todo -> ListViewDataSource
+todoDataSource todos = listViewDataSource $ map todoText todos
+  where todoText (Todo text _) = text
+
+todoList :: Array Todo -> ReactElement
+todoList todos = listView 
+  [appStyle "todoList",
+   renderRow todoRow,
+   dataSource $ todoDataSource todos]
+
+render :: forall props state eff. Render props state eff
+render ctx = pure $ view [(appStyle "container")] [
+  text [(appStyle "title")] [D.text "todos"],
+  todoList initialTodos
+  ]
+        
+  
 main = do
   log "Running app"
   registerComponent appName component
+  unsafeLog $ todoList initialTodos
+  unsafeLog $ todoDataSource initialTodos
   where
     component = createClass viewSpec
     viewSpec = (spec unit render)
